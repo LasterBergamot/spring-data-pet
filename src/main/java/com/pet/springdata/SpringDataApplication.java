955db4c96aedbd2d7955d965022e1fe83f31137d
Andras Torok <>
2020-08-25 15:37:40 +0200

    The Application class now implements CommandLineRunner.

--- src/main/java/com/pet/springdata/SpringDataApplication.java
+++ src/main/java/com/pet/springdata/SpringDataApplication.java
@@ -1,13 +1,18 @@
 package com.pet.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hey!");
+	}
 }
