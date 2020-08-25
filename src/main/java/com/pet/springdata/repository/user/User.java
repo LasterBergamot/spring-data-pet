1b9684af0efdc8142f25a75f6855f6b229c192f1
Andras Torok <>
2020-08-25 09:58:18 +0200

    Created User entity.

---
+++ src/main/java/com/pet/springdata/repository/user/User.java
@@ -0,0 +1,65 @@
+package com.pet.springdata.repository.user;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "firstName", column = @Column(name = "first_name")),
+            @AttributeOverride(name = "middleName", column = @Column(name = "middle_name")),
+            @AttributeOverride(name = "lastName", column = @Column(name = "last_name"))
+    })
+    private Name name;
+
+    @ElementCollection
+    @CollectionTable(name = "phone_numbers", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "phone_numbers")
+    private Set<String> phoneNumbers = new HashSet<>();
+
+    public User() {}
+
+    public User(Name name, Set<String> phoneNumbers) {
+        this.name = name;
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public Set<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(Set<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+}
