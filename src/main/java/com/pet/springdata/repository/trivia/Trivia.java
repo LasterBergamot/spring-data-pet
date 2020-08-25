dd20c39a1123d9cac9bf546845125674df6358e4
Andras Torok <>
2020-08-25 10:03:43 +0200

    Created Trivia entity.

---
+++ src/main/java/com/pet/springdata/repository/trivia/Trivia.java
@@ -0,0 +1,92 @@
+package com.pet.springdata.repository.trivia;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "trivia")
+public class Trivia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    private String category;
+    private String type;
+    private String difficulty;
+    private String question;
+
+    @Column(name = "correct_answer")
+    private String correctAnswer;
+
+    @Column(name = "incorrect_answers")
+    private List<String> incorrectAnswers;
+
+    public Trivia() {}
+
+    public Trivia(String category, String type, String difficulty, String question, String correctAnswer, List<String> incorrectAnswers) {
+        this.category = category;
+        this.type = type;
+        this.difficulty = difficulty;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public List<String> getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(List<String> incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+}
