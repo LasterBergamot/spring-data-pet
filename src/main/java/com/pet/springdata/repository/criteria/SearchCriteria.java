1b845397c5a4fbff97f26f18d5f0884ae2daa6c3
Andras Torok <>
2020-08-27 13:09:50 +0200

    Created SearchCriteria class.

---
+++ src/main/java/com/pet/springdata/repository/criteria/SearchCriteria.java
@@ -0,0 +1,19 @@
+package com.pet.springdata.repository.criteria;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class SearchCriteria {
+
+    @NonNull
+    private final String key;
+
+    @NonNull
+    private final String operation;
+
+    @NonNull
+    private final String value;
+}
