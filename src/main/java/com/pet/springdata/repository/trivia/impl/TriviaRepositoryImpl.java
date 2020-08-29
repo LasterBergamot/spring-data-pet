a0cea15cf15b1287a64224d0ed6bfda9bf987ecb
Andras Torok <>
2020-08-29 13:00:29 +0200

    TriviaRepositoryImpl findTriviaByCategoryTypeAndDifficulty now uses JPA Criteria Metamodels in its queries.

--- src/main/java/com/pet/springdata/repository/trivia/impl/TriviaRepositoryImpl.java
+++ src/main/java/com/pet/springdata/repository/trivia/impl/TriviaRepositoryImpl.java
@@ -2,14 +2,17 @@ package com.pet.springdata.repository.trivia.impl;
 
 import com.pet.springdata.repository.trivia.TriviaRepositoryCustom;
 import com.pet.springdata.repository.trivia.model.Trivia;
+import com.pet.springdata.repository.trivia.model.Trivia_;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
@@ -25,24 +28,25 @@ public class TriviaRepositoryImpl implements TriviaRepositoryCustom {
 
     @Override
     public List<Trivia> findTriviaByCategoryTypeAndDifficulty(String category, String type, String difficulty) {
+        log.info("Finding Trivia with category: {}, type: {}, and difficulty: {}.", category, type, difficulty);
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Trivia> criteriaQuery = criteriaBuilder.createQuery(Trivia.class);
 
         Root<Trivia> triviaRoot = criteriaQuery.from(Trivia.class);
         List<Predicate> predicates = new ArrayList<>();
 
-        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot, "category", category);
-        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot, "type", type);
-        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot, "difficulty", difficulty);
+        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot.get(Trivia_.category), category);
+        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot.get(Trivia_.type), type);
+        addValueToPredicatesIfItIsNotNull(predicates, criteriaBuilder, triviaRoot.get(Trivia_.difficulty), difficulty);
 
         criteriaQuery.where(predicates.toArray(Predicate[]::new));
 
         return entityManager.createQuery(criteriaQuery).getResultList();
     }
 
-    private void addValueToPredicatesIfItIsNotNull(List<Predicate> predicates, CriteriaBuilder criteriaBuilder, Root<Trivia> triviaRoot, String key, String value) {
+    private void addValueToPredicatesIfItIsNotNull(List<Predicate> predicates, CriteriaBuilder criteriaBuilder, Path<String> path, String value) {
         if (value != null) {
-            predicates.add(criteriaBuilder.equal(triviaRoot.get(key), value));
+            predicates.add(criteriaBuilder.equal(path, value));
         }
     }
 }
